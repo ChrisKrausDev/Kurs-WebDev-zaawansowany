030bb06f4384cb400c7a46aae01bfb90d790cf43
Chris <chris.kraus.dev@gmail.com>
2020-11-13 15:53:40 +0100

    day_13 video_86

--- day_12 wprowadzenie do jQuery/85 Projekt 2 zmiana tła i właściwości tekstu po kliknięciu odpowiedniego przycisku/exercise.js
+++ day_12 wprowadzenie do jQuery/85 Projekt 2 zmiana tła i właściwości tekstu po kliknięciu odpowiedniego przycisku/exercise.js
@@ -1,57 +1,59 @@
-$('.interface').on("click", function () {
- // console.log("działa!");
- // console.log(this);
- // console.log($(this))
- // console.log($(this).attr("class"))
- // console.log(this.className)
+$('.interface').on('click', function() {
+    console.log(this);
+        // wypisuje kliknięty el
+    console.log($(this));
+        // wskazany el jest el jQ
+    console.log($(this).attr('class'));
+        // wypisuje klasę wskazanego el
+    console.log(this.className);
+        // wskazanie klasy dla this nie zmienionego na obiekt jQ
 
- if ($(this).hasClass("orange")) {
-  console.log("prawda w pomarańczowy");
-  $('body').attr("class", "orange")
-  // $('body').toggleClass("orange");
-  // $('body').css("background-color", "orange");
-  // $('body').css({
-  //  "background-color": "orange"
-  // })
-  // $('p').css({
-  //  "font-size": "130px",
-  // })
- }
+    if ($(this).hasClass('orange')) {
+        console.log('prawda w pomarańczowy');
+            // sprawdzenie za pomocą jQ czy wskazany element ma klasę orange
+        // $('body').toggleClass('orange');
+            // odwołanie do body i zmiana klasy na orange (tło zmienia się na orange)
+        // $('body').css('background-color', 'orange');
+            // odwołanie się przez .css - zmiana klsy na orange.
 
- if ($(this).hasClass("green")) {
-  console.log("prawda w zielony");
-  $('body').attr("class", "green");
+        // $('body').css({
+        //     'background-color' : 'orange',
+        // })
+            // zmiana koloru poprzez zmianę właściwości css
 
-  // $('body').css({
-  //  "background-color": "#0f0"
-  // })
+        $('body').attr('class', 'orange');
+            // zmiana koloru poprzez zmianę klasy - wygodniejsze rozwiązanie
+    }  
+        
+    // if (this.classList.contains('green')) {
+    //     console.log("prawda w zielony")
+    // }    
+        // sprawdzenie klasy za pomocą JS
 
+    if ($(this).hasClass('green')) {
+        console.log('prawda - w zielony');
+        // $('body').css({
+        //     'background-color' : 'green'
+        // })
+            // mniej wygodne rozwiązanie
 
-  // $('p').css({
-  //  "font-size": "130px",
-  // })
- }
+        $('body').attr('class', 'green');
+    }
+    
+    if($(this).hasClass('increase')) {
+        console.log('prawda w plus');
+        $('.text').animate({
+            'font-size' : '+=10px',
+        }), 500 
+            // animacja! po przecinku - 2000 - ma trwać 500 ms
+    }
 
- if ($(this).hasClass("increase")) {
-  console.log("prawda w plus");
-  $('.text').animate({
-   "font-size": "+=2px"
-  }, 500)
- }
-
- if ($(this).hasClass("move")) {
-  console.log("prawda w strzałkę");
-  $('.text').animate({
-   "left": "+=15px",
-   "letter-spacing": "+=2px"
-  })
- }
-
-
-
-
-
- // if (this.classList.contains("green")) {
- //  console.log("prawda w zielony")
- // }
+    if($(this).hasClass('move')) {
+        console.log('prawda w strzałkę');
+        $('.text').animate({
+            'left' : '+=20px',
+            'letter-spacing' : '+=15px'
+        }), 20 
+    }
 })
+
