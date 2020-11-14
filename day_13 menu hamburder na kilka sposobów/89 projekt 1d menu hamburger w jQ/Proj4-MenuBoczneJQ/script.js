5dfea2827350cf3c794c60434aee2341aa1e6c6e
Chris <chris.kraus.dev@gmail.com>
2020-11-14 13:34:15 +0100

    day_14 video_93

--- day_13 menu hamburder na kilka sposobów/89 projekt 1d menu hamburger w jQ/Proj4-MenuBoczneJQ/script.js
+++ day_13 menu hamburder na kilka sposobów/89 projekt 1d menu hamburger w jQ/Proj4-MenuBoczneJQ/script.js
@@ -1,17 +1,37 @@
-$('.burger').on('click', function () {
-    $('.fas, aside').toggleClass('show');
-})
 
+
+    // przy pomocy jQ 
+
+// $('.burger').on('click', function () {
+//     $('.fas, aside, .wrapper').toggleClass('show');
+// })
+
+    // JS  
 
 // const burger = document.querySelector(".burger");
 
 // const iconBurger = document.querySelector(".fa-bars");
 // const iconX = document.querySelector(".fa-times");
 // const column = document.querySelector("aside");
+// const wrapper = document.querySelector('.wrapper');
 
 // burger.addEventListener("click", function () {
-//     iconBurger.classList.toggle("show");//tak
-//     iconX.classList.toggle("show");//nie
-//     column.classList.toggle("show");//nie
+//     iconBurger.classList.toggle("show"); //tak
+//     iconX.classList.toggle("show"); //nie
+//     column.classList.toggle("show"); //nie
+//     wrapper.classList.toggle('show');
 
 // })
+
+    // sensowniejsze użycie JS:
+
+const burger = document.querySelector('.burger');
+const activeElement = document.querySelectorAll('.active');
+    // pobranie wszystkic el z klasą 'active';
+
+burger.addEventListener('click', function(){
+for(let i = 0; i < activeElement.length; i++) {
+    activeElement[i].classList.toggle('show')
+        // pentla for dla każdego el z klasą 'active' nadaj klasę 'show'goi
+}
+})
--- day_13 menu hamburder na kilka sposobów/91 projekt 2 menu hamburger wysuwające się z góry jQ/script.js
+++ day_13 menu hamburder na kilka sposobów/91 projekt 2 menu hamburger wysuwające się z góry jQ/script.js
@@ -1,6 +1,3 @@
-$(".burger").on("click", function () {
-    $(".fas, nav").toggleClass("off");
+$('.burger').on('click', function(){
+    $('.fas, nav').toggleClass('off');
 })
-
-
-
